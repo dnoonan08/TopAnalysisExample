a980abab81f59f9a23897c2f75bff98c3a352e2a
Daniel C Noonan <dnoonan@blueshark.cluster.fit.edu>
2021-01-25 12:58:50 -0500

    initial commit of environment setup and notebook

---
+++ envSetup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+NAME=ttgenv
+
+# following https://aarongorka.com/blog/portable-virtualenv/, an alternative is https://github.com/pantsbuild/pex
+python3 -m venv --copies $NAME
+source $NAME/bin/activate
+python3 -m pip install setuptools pip --upgrade
+python3 -m pip install jupyterlab
+python3 -m pip install notebook
+#Use master branch of coffea to include jec patch
+python -m pip install git+https://github.com/CoffeaTeam/coffea.git@master
+python -m pip install xxhash
+
+sed -i '40s/.*/VIRTUAL_ENV="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" \&\& pwd)"/' $NAME/bin/activate
+sed -i '1s/#!.*python$/#!\/usr\/bin\/env python/' $NAME/bin/*
+sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
+sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
+
+ipython kernel install --user --name=$NAME
